a3303ef3777c06faa5e7fa4786870483ca2a07d4
ahmedmsaid <dyabco74@gmail.com>
2023-03-24 15:25:09 +0200

    Template-driven forms validation

--- src/app/user/login.component.ts
+++ src/app/user/login.component.ts
@@ -3,7 +3,10 @@ import { AuthService } from "./auth.service";
 import { Router } from "@angular/router"
 
 @Component({
-    templateUrl: './login.component.html'
+    templateUrl: './login.component.html',
+    styles: [`
+        em {float: right; color: red;}
+    `]
 })
 
 export class LoginComponent {
